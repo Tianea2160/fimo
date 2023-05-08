40fb3e791a3f90e61297ba0e24e6829a07c75d0d
tianea2160 <rhlehfndvkd7557@daum.net>
2023-05-08 19:13:48 +0900

    docs : feed controller 문서화

--- src/main/kotlin/com/tianea/fimo/web/feed/FeedController.kt
+++ src/main/kotlin/com/tianea/fimo/web/feed/FeedController.kt
@@ -2,6 +2,12 @@ package com.tianea.fimo.web.feed
 
 import com.tianea.fimo.domain.feed.service.FeedService
 import com.tianea.fimo.domain.post.dto.PostReadDTO
+import com.tianea.fimo.shared.error.ErrorCode
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,6 +20,12 @@ import java.security.Principal
 class FeedController(
     private val feedService: FeedService
 ) {
+    @Operation(summary = "피드 조회", description = "자신과 팔로우한 사용자의 게시글을 조회합니다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "피드 조회 성공"),
+        ]
+    )
     @GetMapping("")
     fun feed(principal: Principal): List<PostReadDTO> = feedService.getFeed(principal.name)
 }
