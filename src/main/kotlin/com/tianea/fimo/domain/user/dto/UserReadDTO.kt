d9345564d4f0c8a036617886f7d5af86a312376b
tianea2160 <hyunjun2087@gmail.com>
2023-07-15 20:26:45 +0900

    fix : add profile image url field in user read dto

--- src/main/kotlin/com/tianea/fimo/domain/user/dto/UserReadDTO.kt
+++ src/main/kotlin/com/tianea/fimo/domain/user/dto/UserReadDTO.kt
@@ -6,13 +6,15 @@ import com.tianea.fimo.domain.user.entity.User
 class UserReadDTO(
     val id: String,
     val nickname: String,
-    val archiveName: String
+    val archiveName: String,
+    val profileImageUrl : String,
 ) {
     companion object {
         fun from(user: User) = UserReadDTO(
             id = user.id,
             nickname = user.nickname,
-            archiveName = user.archiveName
+            archiveName = user.archiveName,
+            profileImageUrl = user.profileImageUrl
         )
     }
 
